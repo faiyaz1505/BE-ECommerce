b4d4695fc251527571ae77199bb3ccfd9d09cc85
faiyaz1505 <fasc786@gmail.com>
2024-03-07 17:43:25 +0530

    roles and users endpoints

---
+++ src/main/java/com/example/entities/Role.java
@@ -0,0 +1,17 @@
+package com.example.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    private String roleName;
+    private String roleDescription;
+}
---
+++ src/main/java/com/example/services/UserService.java
@@ -0,0 +1,17 @@
+package com.example.services;
+
+
+import com.example.entities.User;
+import com.example.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registerNewUser(User user){
+        return userRepository.save(user);
+    }
+}
